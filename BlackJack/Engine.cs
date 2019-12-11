ed2afb43742b229f84d79568c13b550464267a8a
PlusIvan <ivan@ivanmoroz.net>
2019-12-11 10:15:15 +0000

    update

--- BlackJack/Engine.cs
+++ BlackJack/Engine.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
-namespace BlackJack.functions
+namespace BlackJack
 {
     class Engine
     {
@@ -19,53 +19,89 @@ namespace BlackJack.functions
         {
             Render display = new Render(d);
             string[] allow = { "5", "10", "25", "50", "100" };
-            JObject list = new JObject
-        {
-            { "K",  10 },
-            { "Q",  10 },
-            { "J",  10 },
-            { "10",  10},
-            { "9",  9},
-            { "8",  8},
-            { "7",  7},
-            { "6",  6},
-            { "5",  5},
-            { "4",  4},
-            { "3",  3},
-            { "2",  2},
-            { "A",  11}
-        };
-            while (d.balance > 0)
+            while (d.player.balance > 0)
             {
+                display.Show_Logo(d);
+                display.Display();
+                // Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
                 Console.WriteLine($"\nAdd to pot? [5, 10, 25, 50, 100] & when done, type done");
-                string a = Console.ReadLine();
-                if (!allow.Contains(a) && a != "done")
+                string number = Console.ReadLine();
+                if (!allow.Contains(number) && number != "done")
                 {
+                    // Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
                     continue;
                 }
-                else if (allow.Contains(a))
+                else if (number == "done")
                 {
-                    int b = Convert.ToInt32(a);
+                    break;
+                }
+                if (Convert.ToInt32(number) > Convert.ToInt32(d.player.balance))
+                {
+                    //Console.WriteLine($"\nNot enough cash to waste");
+                    continue;
+                }
+                else
+                {
+                    d.player.balance -= Convert.ToInt32(number);
+                    d.pot += Convert.ToInt32(number);
+                    continue;
+                }
+
+
+                /*  
+
                     if (b > Convert.ToInt32(d.balance))
                     {
                         Console.WriteLine($"\nNot enough cash to waste");
                         continue;
                     }
-                    d.balance -= b;
-                    d.pot += b;
-                    display.Display();
-                    continue;
-                }
-                else if (!allow.Contains(a) && a == "done")
-                {
+                    else
+                    {
+                        d.player.balance -= b;
+                        d.pot += b;
+                        display.Display();
+                        continue;
+                    }
+
+
+                }else if (!allow.Contains(a) && a == "done")
+                    {
                     if (d.pot == 0)
                         continue;
 
-                    display.Display();
-                    break;
-                }
+                        display.Display();
+                        break;
+                    }
+                    }*/
             }
-            Console.WriteLine($"\nGame begin?");
+            display.Show_Logo(d);
+            display.Display();
+            // Console.WriteLine($"\nGame begin?");
+            string[] type = { "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
+            string[] symbols = { "s", "c", "d", "h" };
+            //shuffle
+            var deck = new Dictionary<string, int>();
+            while (true)
+            {
+                for (var ty = 0; ty < type.Length; ty++)
+                {
+                    for (var sym = 0; sym < symbols.Length; sym++)
+                    {
+                        if (type[ty] == "A")
+                            deck.Add(type[ty] + symbols[sym], 11);
+                        else if (type[ty] == "K" || type[ty] == "Q" || type[ty] == "J")
+                            deck.Add(type[ty] + symbols[sym], 10);
+                        else
+                            deck.Add(type[ty] + symbols[sym], Convert.ToInt32(type[ty]));
+                    }
+                }
+                break;
+            }
+            foreach (KeyValuePair<string, int> entry in deck)
+            {
+                // do something with entry.Value or entry.Key
+
+            }
         }
     }
 }
--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -13,40 +13,48 @@ namespace BlackJack
 
         static void Main(string[] args)
         {
-            dynamic d = JObject.Parse("{rules:{blackjack:2,dealer_hit:500, surrender:40},dealer:0, pot:0,player:{username:null, balance: 0}}");
+            dynamic d = JObject.Parse("{rules:{blackjack:2,dealer_hit:16, surrender:40},dealer:0, pot:0,player:{username:null, balance: 0}}");
             App app = new App();
             app.Config(true);
             Render display = new Render(d);
+            Engine engine = new Engine(d);
             display.Show_Logo(d);
             display.Display();
             Console.WriteLine($"Type your Username");
             while (d.player.username == null)
             {
-                d.player.username = Console.ReadLine();
+                d.player.username = Console.ReadLine().ToUpper();
                 Console.Title = Console.Title + $" \\/ Welcome {d.player.username}";
                 Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
-          
             }
             display.Show_Logo(d);
             display.Display();
             while (d.player.balance == 0)
             {
-                Console.WriteLine($"How much would you start with, [100-1000]");
+                Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                Console.WriteLine($"\nHow much would you start with, [100-1000]");
                 int amount = 0;
                 try
                 {
                     amount = Convert.ToInt32(Console.ReadLine());
+                    if (amount < 100 || amount > 1000)
+                    {
+                        display.Show_Logo(d);
+                        display.Display();
+                        continue;
+                    }
+
                 }
                 catch
                 {
                     display.Show_Logo(d);
                     display.Display();
-                    Console.WriteLine($"Type a number fuckhead");
                 }
                 d.player.balance = amount;
             }
-            display.Show_Logo(d);
-            display.Display();
+            engine.On();
+
+
             // display.Show_Logo(d);
             //display.Display();
             //  Console.SetCursorPosition((Console.WindowWidth - test.Length) / 2, Console.CursorTop);
